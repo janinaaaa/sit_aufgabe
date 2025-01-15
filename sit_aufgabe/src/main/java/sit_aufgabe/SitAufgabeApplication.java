299863a876e689347664391f4ddb702836ecf607
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-15 14:23:14 +0100

    chore: Initial Spring Boot Project

---
+++ sit_aufgabe/src/main/java/sit_aufgabe/SitAufgabeApplication.java
@@ -0,0 +1,13 @@
+package sit_aufgabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitAufgabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitAufgabeApplication.class, args);
+	}
+
+}
---
+++ sit_aufgabe/src/test/java/sit_aufgabe/SitAufgabeApplicationTests.java
@@ -0,0 +1,13 @@
+package sit_aufgabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitAufgabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
