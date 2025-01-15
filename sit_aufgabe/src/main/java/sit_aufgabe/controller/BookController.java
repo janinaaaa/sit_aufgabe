53fc9148bc8eede62507ed5909512c4237320f64
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-15 17:36:26 +0100

    feat(controller): added controllers for category and book

---
+++ sit_aufgabe/src/main/java/sit_aufgabe/controller/BookController.java
@@ -0,0 +1,51 @@
+package sit_aufgabe.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import sit_aufgabe.dto.AddBookRequest;
+import sit_aufgabe.dto.UpdateBookRequest;
+import sit_aufgabe.model.Book;
+import sit_aufgabe.service.book.BookService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+    @GetMapping("/all")
+    public ResponseEntity<List<Book>> getAllBooks(){
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Integer id){
+        Optional<Book> book = bookService.getBookById(id);
+        return book.isPresent() ? ResponseEntity.ok(book.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/add")
+    public ResponseEntity<Book> addBook(@Valid @RequestBody AddBookRequest book) {
+        return new ResponseEntity<>(bookService.addBook(book), HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PutMapping("/update")
+    public ResponseEntity<Book> updateBook(@Valid @RequestBody UpdateBookRequest book) {
+        return ResponseEntity.ok(bookService.updateBook(book));
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @DeleteMapping("/delete/{id}")
+    public void deleteBook(@PathVariable Integer id){
+        bookService.deleteBook(id);
+    }
+
+}
---
+++ sit_aufgabe/src/main/java/sit_aufgabe/controller/CategoryController.java
@@ -0,0 +1,50 @@
+package sit_aufgabe.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import sit_aufgabe.dto.AddCategoryRequest;
+import sit_aufgabe.dto.UpdateCategoryRequest;
+import sit_aufgabe.model.Category;
+import sit_aufgabe.service.category.CategoryService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+    @GetMapping("/all")
+    public ResponseEntity<List<Category>> getAllBooks(){
+        return ResponseEntity.ok(categoryService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getBookById(@PathVariable Integer id){
+        Optional<Category> category = categoryService.getCategoryById(id);
+        return category.isPresent() ? ResponseEntity.ok(category.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/add")
+    public ResponseEntity<Category> addBook(@Valid @RequestBody AddCategoryRequest category) {
+        return new ResponseEntity<>(categoryService.addCategory(category), HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PutMapping("/update")
+    public ResponseEntity<Category> updateBook(@Valid @RequestBody UpdateCategoryRequest category) {
+        return ResponseEntity.ok(categoryService.updateCategory(category));
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @DeleteMapping("/delete/{id}")
+    public void deleteBook(@PathVariable Integer id){
+        categoryService.deleteCategory(id);
+    }
+}
