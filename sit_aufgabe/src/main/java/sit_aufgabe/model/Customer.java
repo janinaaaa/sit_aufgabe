048f2ec922adf2bb7f8f53536090cdd07cb1e457
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-16 11:03:14 +0100

    feat(model): added roles and made costomer implement user details

--- sit_aufgabe/src/main/java/sit_aufgabe/model/Customer.java
+++ sit_aufgabe/src/main/java/sit_aufgabe/model/Customer.java
@@ -4,18 +4,57 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "Customer")
-public class Customer {
+public class Customer implements UserDetails {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
     private String name;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
---
+++ sit_aufgabe/src/main/java/sit_aufgabe/model/Role.java
@@ -0,0 +1,5 @@
+package sit_aufgabe.model;
+
+public enum Role {
+    CUSTOMER,
+}
