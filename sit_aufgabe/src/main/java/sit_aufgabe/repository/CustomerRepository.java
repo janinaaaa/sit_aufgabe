e31eddc8c9fdce8be85e2b8e9fcc606629cc4e1e
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-16 11:02:50 +0100

    feat(repository): added method to get customers by email

--- sit_aufgabe/src/main/java/sit_aufgabe/repository/CustomerRepository.java
+++ sit_aufgabe/src/main/java/sit_aufgabe/repository/CustomerRepository.java
@@ -4,5 +4,8 @@ package sit_aufgabe.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sit_aufgabe.model.Customer;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Optional<Customer> findByEmail(String email);
 }
